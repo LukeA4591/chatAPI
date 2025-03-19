9e7513aca80716f81184434ddcced57f61553002
Luke Armstrong <lar93@uclive.ac.nz>
2025-03-20 11:51:15 +1300

    Making a chat app api

---
+++ src/app/controllers/user.server.controller.ts
@@ -0,0 +1,72 @@
+import * as users from '../models/user.server.model';
+import Logger from '../../config/logger';
+import {Request, Response} from 'express';
+import {validate} from "../../config/validator";
+import * as schemas from '../../resources/schemas.json';
+
+const list = async (req: Request, res: Response): Promise<void> => {
+    Logger.http(`GET all users`)
+    try {
+        const result = await users.getAll();
+        res.status(200).send(result);
+    } catch (err) {
+        res.status(500)
+            .send(`ERROR getting users ${err}`);
+    }
+};
+
+const create = async (req: Request, res: Response) : Promise<void> => {
+    Logger.http(`POST create a user with username: ${req.body.username}`)
+    const validation = await validate(schemas.user_register, req.body);
+    if (validation !== true) {
+        res.statusMessage = `Bad Request: ${validation.toString()}`;
+        res.status(400).send();
+        return;
+    }
+    const username = req.body.username;
+    try {
+        const result = await users.insert( username );
+        res.status( 201 ).send({"user_id": result.insertId} );
+    } catch( err ) {
+        res.status( 500 ).send( `ERROR creating user ${username}: ${ err }`);
+    }
+};
+
+const read = async (req: Request, res: Response): Promise<void> => {
+    Logger.http(`GET single user id: ${req.params.id}`)
+    const id = req.params.id;
+    try {
+        const result = await users.getOne(parseInt(id, 10));
+        if (result.length === 0) {
+            res.status(404).send('User not found');
+        } else {
+            res.status(200).send(result[0]);
+        }
+    } catch( err ) {
+        res.status( 500 ).send(`ERROR reading user ${id}: ${ err }`);
+    }
+};
+
+const update = async (req: Request, res: Response): Promise<void> => {
+    Logger.http(`PUT update user with id: ${req.params.id}`)
+    const validation = await validate(schemas.user_register, req.body);
+    if (validation !== true) {
+        res.statusMessage = `Bad Request: ${validation.toString()}`;
+        res.status(400).send();
+        return;
+    }
+    const id = req.params.id;
+    const username = req.body.username;
+    try {
+        const result = users.alter(parseInt(id, 10), username);
+
+    } catch ( err ) {
+        res.status( 500 ).send(`ERROR updating user ${id}: ${ err }`);
+    }
+};
+
+const remove = async (req: Request, res: Response): Promise<void> => {
+    return null;
+    }
+
+export { list, create, read, update, remove }
---
+++ src/config/db.ts
@@ -0,0 +1,34 @@
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+import Logger from './logger';
+dotenv.config();
+
+const state = {
+    // @ts-ignore
+    pool: null
+};
+
+const connect = async (): Promise<void> => {
+    state.pool = await mysql.createPool({
+        host: process.env.SENG365_MYSQL_HOST,
+        user: process.env.SENG365_MYSQL_USER,
+        password: process.env.SENG365_MYSQL_PASSWORD,
+        database: process.env.SENG365_MYSQL_DATABASE,
+        ssl: {
+            rejectUnauthorized: false
+            }
+        });
+    await state.pool.getConnection(); // Check connection
+    Logger.info(`
+ Successfully connected to database`
+    )
+    return
+
+     };
+
+const getPool = () => {
+
+    return state.pool;
+     };
+
+ export {connect, getPool}
